674e1380f24127aa74056fb999569173174670f1
Bartłomiej Kufel <joloczok123@gmail.com>
2024-11-06 17:19:28 +0100

    Update Source.c

--- Lab04_11/Source.c
+++ Lab04_11/Source.c
@@ -8,6 +8,23 @@
 
 int main() {
 
+	//zad 1 i zad 2
+
+	/*int tab[10];
+	int k = 3;
+
+	for (int i = 0; i < 10; i++)
+	{
+		tab[i] = k;
+		k += 3;
+		if ((i + 1) % 2 == 1)
+			printf("%d\t", tab[i]);
+	
+	}*/
+	
+
+
+
 	//zad 3
 	/*printf("Podaj a: ");
 	int a = 0;
@@ -56,6 +73,86 @@ int main() {
 	{
 		printf("%d\t", tab[i]);
 	}*/
+
+	//zad 6
+
+	/*int tab[20] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < 10; i++)
+	{
+		tab[i + 10] = tab[i];
+	}
+
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d\t",tab[i]);
+	}
+	*/
+
+	//zad 7
+
+	/*int macierz[3][3] = { 1,1,1,2,2,2,3,3,3 };
+	int suma = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (i == j)
+				suma += macierz[i][j];
+
+			printf("%d \t",macierz[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\nSuma przekatnej: %d", suma);*/
+
+	//zad 8
+
+	int macierz1[3][3] = { 1,1,1,2,2,2,3,3,3 };
+	int macierz2[3][3] = { 4,4,4,5,5,5,6,6,6 };
+
+	int macierzSuma[3][3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			macierzSuma[i][j] = macierz1[i][j] + macierz2[i][j];
+		}
+	}
+
+	printf("\nMacierz nr. 1:\n");
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d\t",macierz1[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\nMacierz nr. 2:\n");
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d\t", macierz2[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\nMacierze po dodaniu:\n");
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d\t", macierzSuma[i][j]);
+		}
+		printf("\n");
+	}
 
 
 
